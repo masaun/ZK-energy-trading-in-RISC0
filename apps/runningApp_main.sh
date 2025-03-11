7547b5fa2874f7ecbd0de02c59f2571d46505889
masaun <masaun@protonmail.com>
2025-03-11 22:17:28 +0900

    [apps]: Add some comments

--- apps/runningApp_main.sh
+++ apps/runningApp_main.sh
@@ -1,5 +1,6 @@
 echo "Read the environment variables"
 . ./.env # load the environment variables from the .env file for deployment
 
-echo "Running the app (./apps/src/main.rs)"
-RUST_LOG=info cargo run --bin app -- --even-number-address ${EVEN_NUMBER_ADDRESS:?} --number 4
+echo "Running the app (./apps/src/main.rs) with the following environment variables:"
+RUST_LOG=info cargo run --bin app -- --even-number-address ${EVEN_NUMBER_ADDRESS:?} --number 5
+#RUST_LOG=info cargo run --bin app -- --even-number-address ${EVEN_NUMBER_ADDRESS:?} --number 4
