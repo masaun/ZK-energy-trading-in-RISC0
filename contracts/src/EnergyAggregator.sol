40b5afb18d812629ce04cd78532f5678410bfbc1
masaun <masaun@protonmail.com>
2025-03-12 19:47:02 +0900

    [SC]: Define the submitEnergyData() and the getEnergyData() in the EnergyAggregator.sol

--- contracts/src/EnergyAggregator.sol
+++ contracts/src/EnergyAggregator.sol
@@ -5,5 +5,37 @@ import { ImageID } from "./ImageID.sol"; // auto-generated contract after runnin
 
 /// @title the Energy Aggregator contract
 contract EnergyAggregator {
+    /// @notice RISC Zero verifier contract address.
+    IRiscZeroVerifier public immutable verifier;
+    /// @notice Image ID of the only zkVM binary to accept verification from.
+    ///         The image ID is similar to the address of a smart contract.
+    ///         It uniquely represents the logic of that guest program,
+    ///         ensuring that only proofs generated from a pre-defined guest program
+    ///         (in this case, checking if a number is even) are considered valid.
+    bytes32 public constant imageId = ImageID.IS_SMART_MATER_ID;
+
+    /// @notice A number that is guaranteed, by the RISC Zero zkVM, to be even.
+    ///         It can be set by calling the `set` function.
+    uint256 public number;
+
+    /// @notice Initialize the contract, binding it to a specified RISC Zero verifier.
+    constructor(IRiscZeroVerifier _verifier) {
+        verifier = _verifier;
+        number = 0;
+    }
+
+    /// @notice Set the even number stored on the contract. Requires a RISC Zero proof that the number is even.
+    function submitEnergyData(uint256 x, bytes calldata seal) public { /// @dev - Submitted by a Producer.
+        // Construct the expected journal data. Verify will fail if journal does not match.
+        bytes memory journal = abi.encode(x);
+        verifier.verify(seal, imageId, sha256(journal));
+        number = x;
+    }
+
+    /// @notice Returns the number stored.
+    function getEnergyData() public view returns (uint256) {
+        return number;
+    }
+
     /// [TODO]:
 }
